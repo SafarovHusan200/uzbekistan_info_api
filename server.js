ccec05ac57b11fd5e116e8ecbdd8bad050d51ae2
SafarovHusan200 <safarovhusan2000@gmail.com>
2023-04-29 20:50:26 +0500

    cors

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express();
 // Body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.cors();
+app.use(cors());
 
 // Statis folder
 app.use(express.static(path.join(__dirname, "public")));
